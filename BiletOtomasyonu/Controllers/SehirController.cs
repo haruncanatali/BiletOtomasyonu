eded73152cbdf28955a47f10653a151111144065
Haruncan Atalı <77530565+haruncanatali@users.noreply.github.com>
2021-12-21 23:36:50 +0300

    güncelleme

---
+++ BiletOtomasyonu/Controllers/SehirController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BiletOtomasyonu.Controllers
+{
+    public class SehirController : Controller
+    {
+        // GET: Sehir
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
